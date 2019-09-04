e02bb0b7309d489497f4bca9b92857b5b8636491
Braden Fullmer <46699133+bradenfullmer@users.noreply.github.com>
2019-09-04 11:30:39 -0600

    Utilities

---
+++ Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities 
+{
+
+    public static string ProcessText(string textIn)
+    {
+        if(textIn != null)
+        {
+            try
+            {
+                float numbers;
+                numbers = float.Parse(textIn);
+                textIn = "You entered the number: " + numbers;
+                return textIn;
+            }
+            catch
+            {
+                return ("The string you entered was: " + textIn);
+            }
+        }
+        return null;
+    }
+}
