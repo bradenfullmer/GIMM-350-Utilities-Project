61bd4cdb270eae0dd78c3f60db26b5731a1a4d26
Braden Fullmer <46699133+bradenfullmer@users.noreply.github.com>
2019-09-13 17:16:31 -0600

    Create Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    none,
+    blaster,
+    spread,
+    phaser,
+    missile,
+    laser,
+    shield
+}
+
+[System.Serializable]
+public class WeaponDefinition
+{
+    public WeaponType type = WeaponType.none;
+    public string letter;
+    public Color color = Color.white;
+    public GameObject projectilePrefab;
+    public GameObject laserPrefab;
+    public Color projectileColor = Color.white;
+    public float damageOnHit = 0;
+    public float continousDamage = 0;
+    public float delayBetweenShots = 0;
+    public float velocity = 20;
+}
+public class Weapon : MonoBehaviour
+{
+    static public Transform PROJECTILE_ANCHOR;
+
+    [Header("Set Dynamically")] [SerializeField]
+    private WeaponType _type = WeaponType.none;
+    public WeaponDefinition def;
+    public GameObject collar;
+    public float lastShotTime;
+    private Renderer collarRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collar = transform.Find("Collar").gameObject;
+        collarRend = collar.GetComponent<Renderer>();
+
+        SetType(_type);
+
+        if(PROJECTILE_ANCHOR == null)
+        {
+            GameObject go = new GameObject("_ProjectileAnchor");
+            PROJECTILE_ANCHOR = go.transform;
+        }
+
+        GameObject rootGO = transform.root.gameObject;
+        if (rootGO.GetComponent<Hero>() != null)
+        {
+            rootGO.GetComponent<Hero>().fireDelegate += Fire;
+        }
+    }
+
+    public WeaponType type
+    {
+        get { return (_type); }
+        set { SetType(value); }
+    }
+
+    public void SetType(WeaponType wt)
+    {
+        _type = wt;
+        if(type == WeaponType.none)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+        def = Main.GetWeaponDefinition(_type);
+        collarRend.material.color = def.color;
+        lastShotTime = 0;
+    }
+
+    public void Fire()
+    {
+        if (!gameObject.activeInHierarchy) return;
+
+        if(Time.time - lastShotTime < def.delayBetweenShots)
+        {
+            return;
+        }
+        Projectile p;
+        Vector3 vel = Vector3.up * def.velocity;
+        if(transform.up.y < 0)
+        {
+            vel.y = -vel.y;
+        }
+
+        switch (type)
+        {
+            case WeaponType.blaster:
+                p = MakeProjectile();
+                p.rigid.velocity = vel;
+                break;
+            case WeaponType.spread:
+                p = MakeProjectile();
+                p.rigid.velocity = vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(10, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
+                p = MakeProjectile();
+                p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
+                p.rigid.velocity = p.transform.rotation * vel;
+                break;
+                //adding in a new weapon type, a slow firing missile
+            case WeaponType.missile:
+                p = MakeProjectile();
+                p.rigid.velocity = vel;
+                break;
+                //adding a new weapon type, a long laser blast
+            case WeaponType.laser:
+                p = MakeProjectileLaser();
+                p.rigid.velocity = vel;
+                break;
+        }
+    }
+
+    public Projectile MakeProjectile()
+    {
+        GameObject go = Instantiate(def.projectilePrefab);
+        if(transform.parent.gameObject.tag == "Hero")
+        {
+            go.tag = "ProjectileHero";
+            go.layer = LayerMask.NameToLayer("ProjectileHero");
+        }
+        else
+        {
+            go.tag = "ProjectileEnemy";
+            go.layer = LayerMask.NameToLayer("ProjectileEnemy");
+        }
+        go.transform.position = collar.transform.position;
+        go.transform.SetParent(PROJECTILE_ANCHOR, true);
+        Projectile p = go.GetComponent<Projectile>();
+        p.type = type;
+        lastShotTime = Time.time;
+        return (p);
+    }
+
+     public Projectile MakeProjectileLaser()
+    {
+        GameObject go = Instantiate(def.laserPrefab);
+        if(transform.parent.gameObject.tag == "Hero")
+        {
+            go.tag = "ProjectileHero-Laser";
+            go.layer = LayerMask.NameToLayer("ProjectileHero-Laser");
+        }
+        else
+        {
+            go.tag = "ProjectileEnemy";
+            go.layer = LayerMask.NameToLayer("ProjectileEnemy");
+        }
+        go.transform.position = collar.transform.position;
+        go.transform.SetParent(PROJECTILE_ANCHOR, true);
+        Projectile p = go.GetComponent<Projectile>();
+        p.type = type;
+        lastShotTime = Time.time;
+        return (p);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
